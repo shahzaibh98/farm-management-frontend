35f90e994dcdedd4de8eda20b79c9556e14891ca
shahzaibh98 <shahzaib.hassan@sybrid.com>
2024-03-20 12:23:05 +0500

    error boundary is added

---
+++ src/error-boundary.tsx
@@ -0,0 +1,40 @@
+import { Component, ErrorInfo, ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false,
+  };
+
+  public static getDerivedStateFromError(someError: Error): State {
+    // Update state so the next render will show the fallback UI.
+    console.log('Error boundary caught an error:', someError);
+    return { hasError: true };
+  }
+
+  /**
+   * TODO: Add loggin errors to the server functionality here
+   * @param error
+   * @param errorInfo
+   */
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('Uncaught error:', error, errorInfo);
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return <h3>Something went wrong...</h3>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
