77fa7e41ff184c0bfd3a6ac196bd358de9191240
shahzaibh98 <shahzaib.hassan@sybrid.com>
2024-03-20 10:46:30 +0500

    Revert "inital setup completed"
    This reverts commit a3adae6a5fd12a4d00896b9bacb8cb1d62ab6a8b.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,7 @@
-import { useSelector } from 'react-redux';
-
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline, StyledEngineProvider } from '@mui/material';
-
-// routing
-import Routes from 'routes';
-
-// defaultTheme
-import themes from 'themes';
-
-// project imports
-import NavigationScroll from 'layout/NavigationScroll';
-
-// ==============================|| APP ||============================== //
-
-const App = () => {
-  const customization = useSelector(
-    (state: { customization: any }) => state.customization
-  );
-
-  return (
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={themes(customization)}>
-        <CssBaseline />
-        <NavigationScroll>
-          <Routes />
-        </NavigationScroll>
-      </ThemeProvider>
-    </StyledEngineProvider>
-  );
-};
+import './App.css';
+import React from 'react';
+function App() {
+  return <div className="text-3xl font-bold">Farm Management</div>;
+}
 
 export default App;
