c1c105dd2153c1dc41762357f450f383d43c1407
shahzaibh98 <shahzaib.hassan@sybrid.com>
2024-04-26 18:23:07 +0500

    calendar fix

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,23 +1,51 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import {
   Navigate,
   RouterProvider,
   createBrowserRouter,
 } from 'react-router-dom';
-import { getRoutesAgainstRole } from './role-based.routes';
+import { systemRoles } from '../utils/common/constant.objects';
+import {
+  cropRoutes,
+  dashboardRoutes,
+  farmAdminRoutes,
+  financialRoutes,
+  inventoryRoutes,
+  livestockRoutes,
+  profileRoutes,
+  publicRoutes,
+  taskRoutes,
+} from './route';
 
 const AppRouter: React.FC = () => {
+  const isSuperAdmin =
+    useSelector((state: any) => state?.userInfo?.userInfo)?.roleId ===
+    systemRoles[0].id;
+
   const authRouter = createBrowserRouter(
-    [
-      ...getRoutesAgainstRole(),
-      {
-        path: '*',
-        element: <Navigate to="/" />,
-      },
-    ],
-    {
-      basename: '/',
-    }
+    isSuperAdmin
+      ? [
+          ...farmAdminRoutes,
+          ...dashboardRoutes,
+          ...profileRoutes,
+          ...publicRoutes,
+        ]
+      : [
+          ...taskRoutes,
+          ...cropRoutes,
+          ...livestockRoutes,
+          ...publicRoutes,
+          ...dashboardRoutes,
+          ...financialRoutes,
+          ...inventoryRoutes,
+          ...profileRoutes,
+          {
+            path: '*',
+            element: <Navigate to="/" />,
+          },
+        ],
+    { basename: '/' }
   );
 
   return <RouterProvider router={authRouter} />;
