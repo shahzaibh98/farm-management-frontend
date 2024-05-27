a998e67989eac2e193d2d2776146dd4b412957ed
shahzaibh98 <shahzaib.hassan@sybrid.com>
2024-05-27 20:40:49 +0500

    land routes is updated

--- src/routes/route/land.routes.tsx
+++ src/routes/route/land.routes.tsx
@@ -1,10 +1,12 @@
 import { DocumentTitle } from '../../concave.agri/components/title';
 import { AppShell } from '../../pages/app-layout';
 import { ErrorPage } from '../../pages/error';
-import ManageLand from '../../pages/land/land.foam';
+import ManageLand from '../../pages/land/land/land.foam';
 import BedsView from '../../pages/land/beds/view.beds';
-import LandView from '../../pages/land/view.land';
+import LandView from '../../pages/land/land/view.land';
 import PrivateRoute from '../private/private.routes';
+import ManageBed from '../../pages/land/beds/beds.from';
+import SoilTestView from '../../pages/land/soil-test/view-soil-tests.page';
 
 export const landRoutes = [
   {
@@ -71,7 +73,7 @@ export const landRoutes = [
         element: (
           <PrivateRoute>
             <DocumentTitle title="Land - View" />
-            <ManageLand type="View" />
+            <ManageBed type="View" apiEndPoint="bed" pageLabel="Beds" />
           </PrivateRoute>
         ),
       },
@@ -80,7 +82,51 @@ export const landRoutes = [
         element: (
           <PrivateRoute>
             <DocumentTitle title="Land - Edit" />
-            <ManageLand type="Update" />
+            <ManageBed type="Update" apiEndPoint="bed" pageLabel="Beds" />
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+  {
+    path: 'soil-tests',
+    element: <AppShell />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        path: ':id',
+        element: (
+          <PrivateRoute>
+            <DocumentTitle title="Soil Test" />
+            <SoilTestView />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'add:id',
+        element: (
+          <PrivateRoute>
+            <DocumentTitle title="Soil Test - Add" />
+            {/* <CropForm apiEndPoint="crop" pageLabel="Crops" /> */}
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'edit/:id',
+        element: (
+          <PrivateRoute>
+            <DocumentTitle title="Soil Test - Edit" />
+            {/* <CropForm type="Update" apiEndPoint="crop" pageLabel="Crops" /> */}
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'view/:id',
+        element: (
+          <PrivateRoute>
+            <DocumentTitle title="Soil Test - View" />
+            {/* <CropForm type="View" apiEndPoint="crop" pageLabel="Crops" /> */}
           </PrivateRoute>
         ),
       },
